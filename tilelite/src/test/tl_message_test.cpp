9ad3ec8cd9275c884f6e86d7990ae26ab1ac7546
Siim Kallas <siimkallas@gmail.com>
2016-04-23 23:15:29 +0300

    Fixed i32 multi read unit test. Added byte reading, test.

--- tilelite/src/test/tl_message_test.cpp
+++ tilelite/src/test/tl_message_test.cpp
@@ -44,13 +44,29 @@ const char* i32_multi() {
   mu_assert("i32 read", tlm_read_i32(&m) == 598438445);
   mu_assert("i32 next end", tlm_next(&m) == tlm_none);
   mu_assert("i32 next x2 end", tlm_next(&m) == tlm_none);
+  return NULL;
 }
+
+const char* byte_read() {
+  const uint8_t msg[] = {'b', 'b', 'b', '\0', 0x2, 0x4, 0x8 };
+  tl_message m;
+  mu_assert("byte read start", tlm_start(msg, sizeof(msg), &m) == 1);
+  mu_assert("byte next", tlm_next(&m) == tlm_byte);
+  mu_assert("byte read", tlm_read_byte(&m) == 0x2);
+  mu_assert("byte next", tlm_next(&m) == tlm_byte);
+  mu_assert("byte read", tlm_read_byte(&m) == 0x4);
+  mu_assert("byte next", tlm_next(&m) == tlm_byte);
+  mu_assert("byte read", tlm_read_byte(&m) == 0x8);
+  mu_assert("byte next end", tlm_next(&m) == tlm_none);
+  return NULL;
+};
 
 static const char* tests() {
   mu_run_test(empty_string);
   mu_run_test(short_string);
   mu_run_test(i32_string);
   mu_run_test(i32_multi);
+  mu_run_test(byte_read);
   return NULL;
 }
 
--- tilelite/src/tl_message.cpp
+++ tilelite/src/tl_message.cpp
@@ -43,6 +43,14 @@ tl_message_type tlm_next(tl_message* message) {
   return tlm_type(*message->tag_cursor++);
 }
 
+uint8_t tlm_read_byte(tl_message* message) {
+  uint8_t result = *message->data_cursor;
+
+  message->data_cursor++;
+
+  return result;
+}
+
 int32_t tlm_read_i32(tl_message* message) {
   const uint8_t* p = message->data_cursor;
   int32_t res = int32_t(p[0]) << 24 | int32_t(p[1]) << 16 | int32_t(p[2]) << 8 |
